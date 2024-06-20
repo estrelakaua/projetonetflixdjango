43ae590be64fc95eb24b613830a19ebd8aa72c24
kaua <kauastrela@gmail.com>
2024-06-20 13:47:40 -0300

    debug para true

--- estrelaflix/settings.py
+++ estrelaflix/settings.py
@@ -32,7 +32,7 @@ else:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['projetonetflixdjango-production-ac82.up.railway.app', 'localhost', '127.0.0.1']
 
